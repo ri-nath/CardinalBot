6084e0e176a589f162b0f2e320eaa8de3cefc7f3
rishinath4159 <rishi.nath@gmail.com>
2018-12-31 14:26:19 -0800

    Fixed decode command

--- commands/decode.js
+++ commands/decode.js
@@ -4,21 +4,13 @@ const Discord = require("discord.js");
 
 exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed();
-    console.log(args[0]);
 
-    if (args.length < 1){
-      args[0] = "HELP";
-    }
+    if (args.length < 1 || !(args[0] in codes)){args[0] = "HELP";}
 
     args[0] = args[0].toUpperCase();
-
-    if (args[0] == "CODES" || args[0] == "HELP"){
-        embed.setAuthor("Decode Command Info");
-    } else {
-        embed.setAuthor("Schedule for code" + args[0], client.user.avatarURL);
-    }
-    embed.setTitle(codes[args][0])
-    .setDescription(codes[args][1])
+    if (args[0] == "CODES" || args[0] == "HELP"){embed.setAuthor("Decode Command Info");}
+    else {embed.setAuthor(codes[args[0]][0].slice(2, codes[args[0]][0].length - 2), client.user.avatarURL);}
+    embed.setDescription(codes[args[0]][1])
     .setColor(8989999)
     .setTimestamp();
 
